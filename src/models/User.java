4fc7f2f76f8b7a6e890f8af428c3d9f8863c6fa2
Luke Halley <luke123halley@gmail.com>
2016-12-06 16:56:06 +0000

    Added XSTream

--- src/models/User.java
+++ src/models/User.java
@@ -26,25 +26,4 @@ public class User {
 		this.zipCode = zipCode;
 	}
 
-//	public String toString() {
-//		return new ToJsonString(getClass(), this).toString();
-//	}
-//
-//	@Override
-//	public int hashCode() {
-//		return Objects.hashCode(this.id, this.firstName, this.lastName, this.gender, this.age, this.occupation, this.zipCode);
-//	}
-//
-//	@Override
-//	public boolean equals(final Object obj) {
-//		if (obj instanceof User) {
-//			final User other = (User) obj;
-//			return Objects.equal(firstName, other.firstName) && Objects.equal(lastName, other.lastName)
-//					&& Objects.equal(gender, other.gender) && Objects.equal(age, other.age)
-//					&& Objects.equal(occupation, other.occupation) && Objects.equal(ratings, other.ratings);
-//		} else {
-//			return false;
-//		}
-//	}
-
 }
