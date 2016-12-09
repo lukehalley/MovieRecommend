c2f7d0c48702d8021fe49ccf838ddd4e0716592c
Luke Halley <luke123halley@gmail.com>
2016-12-09 15:49:59 +0000

    Serialiser

---
+++ src/utils/XMLSerializer.java
@@ -0,0 +1,67 @@
+package utils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Stack;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import models.Movie;
+import models.Rating;
+import models.User;
+
+@SuppressWarnings("serial")
+public class XMLSerializer implements Serializable {
+
+	@SuppressWarnings("rawtypes")
+	private static Stack stack = new Stack();
+	private File file;
+
+	public XMLSerializer(File file) {
+		this.file = file;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void push(Object o) {
+		stack.push(o);
+	}
+
+	public static Object pop(Map<Long, Movie> movies, Map<Long, Rating> ratings, Map<Long, User> users) {
+		return stack.pop();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void read() throws Exception {
+		ObjectInputStream is = null;
+
+		try {
+			XStream xstream = new XStream(new DomDriver());
+			is = xstream.createObjectInputStream(new FileReader("serialisedData.xml"));
+			stack = (Stack) is.readObject();
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
+
+	public void write() throws Exception {
+		ObjectOutputStream os = null;
+
+		try {
+			XStream xstream = new XStream(new DomDriver());
+			os = xstream.createObjectOutputStream(new FileWriter("serialisedData.xml"));
+			os.writeObject(stack);
+		} finally {
+			if (os != null) {
+				os.close();
+			}
+		}
+	}
+}
